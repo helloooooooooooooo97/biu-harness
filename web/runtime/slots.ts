601ca167609a5214f070b2ff98d092f0cb38c515
97 <348876414@qq.com>
2026-08-17 09:20:33 +0800

    chore: remove old Cordis demo to start deepseek-harness course

--- web/runtime/slots.ts
+++
@@ -1,8 +0,0 @@
-export { SlotsService, name, inject, apply } from '../ui-slots/index.ts'
-export type {
-  RegisterOptions,
-  SlotComponent,
-  SlotRenderer,
-  SlotRendererHost,
-  StoredEntry,
-} from '../ui-slots/index.ts'
--- web/types.ts
+++
@@ -1,9 +0,0 @@
-import type { SlotsService } from './ui-slots/index.ts'
-import type { SnapshotService } from './runtime/snapshot.ts'
-
-declare module 'cordis' {
-  interface Context {
-    slots: SlotsService
-    snapshot: SnapshotService
-  }
-}
